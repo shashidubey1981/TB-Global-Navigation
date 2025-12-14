707e47cb83d5f55ad70b0d6f3740b49fc55335fe
Shashi <shashidubey1981@gmail.com>
2025-12-13 17:18:31 -0800

    build issues

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 /**
  * Main entry point for tb-global-navigation package
- * Export Header component and related types for use in other Next.js applications
  */
-
-// Export Header component and types
-export { default as Header } from '../components/header/Header';
-export { default as Footer } from '../components/footer/Footer';
-
-// Export all header sub-components
-export * from '../components/header';
-// Export Footer component and types
-export * from '../components/footer';
